57c78f99c9b09634d31f6573ffba6ce1f4ffe07c
achabill <achabill12@gmail.com>
2016-11-09 15:34:00 +0100

    initial admin app

---
+++ adminapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vodadminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vodadminApp
+ */
+angular.module('vodadminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
