7857fae09bfbe0e2dc0be824cf620ed5876f4bc8
achabill <achabill12@gmail.com>
2016-11-17 14:23:04 +0100

    movies controller

--- adminapp/app/scripts/controllers/moviesController.js
+++ adminapp/app/scripts/controllers/moviesController.js
@@ -1,40 +1,90 @@
 /// <reference path=".././../../typings/index.d.ts" />
 'use strict';
 
-angular.module('vodadminApp')
-.controller('MoviesController',['UserService', '$location',function(userService, $location){
+angular.module('vodadminApp').controller('MoviesController', ['UserService', 'MoviesService', '$location', function (userService, moviesService, $location) {
     self = this;
-        
+    self.partial;
     self.user = userService.user;
-    if(self.user == null)
+
+    if (self.user == null)
         $location.path("/");
 
-    self.getAllMovies = function(){
-            
+    self.getAllMovies = function () {
+        moviesService.getAllMovies().then(function (response) {
+            self.response = response.data;
+            self.partial = 'get-all-movies';
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.postMovie = function(){
-
+    self.postMovie = function () {
+        var date = self.newMovie.releaseyear;
+        date = date.toString().replace('/','-');
+        self.newMovie.releaseyear = date;
+        var year = self.newMovie.releaseyear.date.getUTCFullYear();
+        self.newMovie.releaseyear = year;
+        console.log(self.newMovie);
+        moviesService.postMovie(self.newMovie).then(function (response) {
+            self.partial = 'post-a-movie';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.deleteAllMovies = function(){
-
+    self.deleteAllMovies = function () {
+        moviesService.deleteAllMovies().then(function (response) {
+            self.partial = 'delete';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.getMoviebyId = function(id){
-
+    self.getMoviebyId = function () {
+        moviesService.getMoviebyId(self.movieId).then(function (response) {
+            self.partial = 'get-movie-by-id';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.deleteMovieybyId = function(id){
-
+    self.deleteMovieybyId = function () {
+        moviesService.deleteMovieybyId(self.movieId).then(function (response) {
+            self.partial = 'delete';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.getAllMovieComments = function(id){
-
+    self.getAllMovieComments = function () {
+        moviesService.getAllMovieComments(self.movieId).then(function (response) {
+            self.partial = 'get-movie-comments';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.deleteAllMovieComments = function(id){
-
+    self.deleteAllMovieComments = function () {
+        moviesService.deleteAllMovieComments(self.movieId).then(function (response) {
+            self.partial = 'delete';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.getCommentbyId = function(movieId, commentId){
-
+    self.getCommentbyId = function () {
+        moviesService.getCommentbyId(self.movieId, self.commentId).then(function (response) {
+            self.partial = 'get-movie-comment-by-id';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-    self.deleteCommentbyId = function(movieId, commentId){
-
+    self.deleteCommentbyId = function () {
+        moviesService.deleteCommentbyId(self.movieId, self.commentId).then(function (response) {
+            self.partial = 'delete';
+            self.response = response.data;
+        }, function (error) {
+            console.log(error.data.message);
+        });
     };
-        
 }]);
--- adminapp/app/scripts/services/moviesService.js
+++ adminapp/app/scripts/services/moviesService.js
@@ -2,38 +2,79 @@
 
 'use strict';
 
-angular.module('vodadminApp')
-.factory('MoviesService',['$http', '$q','UserService',function($http, $q, userService){
+angular.module('vodadminApp').factory('MoviesService', ['$http', '$q', 'UserService', function($http, $q, userService) {
     var baseEndPoint = 'http://localhost:8080/admin/movies';
-    var service =  {
-        accesstoken: userService.accesstoken,
-        error: null,
-        getAllMovies: function(){
-            return $http.post(baseEndPoint + '/').then(function(response){
-                service.accesstoken = response.data.accessToken.accesstoken;
-                service.user = response.data.user;
-            }, function(error){
-                service.error = error.data.message;
+    var service = {
+        getAllMovies: function() {
+            return $http.get(baseEndPoint + '/?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
                 return $q.reject(error);
             });
         },
-        signup: function(user){
-            return $http.post(baseEndPoint + '/signup', user).then(function(response){
-                service.isLoggedin = true;
-                service.accesstoken = response.data.accessToken.accesstoken;
-                service.user = response.data.user;
-            }, function(error){
-                service.error = error.data.message;
+        postMovie: function(movie) {
+            console.log('here', movie.title);
+            return $http.post(baseEndPoint + '/', movie + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
                 return $q.reject(error);
             });
         },
-        logout: function(){
-            return $http.get(baseEndPoint + '/logout?accesstoken='+ service.accesstoken).then(function(){},
-            function(error){
-                service.error = error.data.message;
-                console.log(error.data);
+        deleteAllMovies: function() {
+            return $http.delete(baseEndPoint + '/' + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
                 return $q.reject(error);
             });
+        },
+        getMoviebyId: function(id) {
+            return $http.get(baseEndPoint + '/' + id + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        deleteMovieybyId: function(id) {
+            return $http.delete(baseEndPoint + '/' + id + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        getAllMovieComments: function(id) {
+            return $http.get(baseEndPoint + '/' + id + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        deleteAllMovieComments: function(id) {
+            return $http.delete(baseEndPoint + '/' + id + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        getCommentbyId: function(movieId, commentId) {
+            return $http.get(baseEndPoint + '/' + movieId + '/' + commentId + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        deleteCommentbyId: function(movieId, commentId) {
+            return $http.delete(baseEndPoint + '/' + movieId + '/' + commentId + '?accesstoken=' + userService.accesstoken).then(function(response) {
+                return $q.when(response);
+            }, function(error) {
+                return $q.reject(error);
+            });
+        },
+        playMovie: function(id){
+            return $http.get('http://localhost:8080/movies/' + id + '/?play=true').then(function(response){
+                return $q.when(response);
+            },function(error){
+                return $q.reject(error);
+            })
         }
     };
     return service;
